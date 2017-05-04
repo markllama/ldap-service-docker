ebabcb14a5a1c83380e4254003250990943a4458
Mark Lamourine <markllama@gmail.com>
2017-05-03 21:03:18 -0400

    added procps and db setups with mdb

--- startup.sh
+++ startup.sh
@@ -12,66 +12,76 @@ fi
 
 # Convert inputs to LDAP DN form
 SUFFIX=$(echo $DOMAIN | sed -e 's/^/dc=/' -e 's/\./,dc=/g')
+ROOT_DC=$(echo $DOMAIN | cut -d. -f1)
 ADMIN_DN=cn=${ADMIN_USERNAME},${SUFFIX}
+ADMIN_HASH=$(slappasswd -s ${ADMIN_PASSWORD})
 
-PW_HASH=$(slappasswd -s ${ADMIN_PASSWORD})
+function set_suffix() {
+    # SUFFIX=$1
 
-#function set_suffix() {
-#    # $1 = SUFFIX
-#    
-#}
+    ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}mdb,cn=config
+changetype: modify
+replace: olcSuffix
+olcSuffix: $1
+EOF
 
-#function set_admin_name() {
-#
-#}
+}
 
-#function set_admin_password() {
-#
-#}
+function set_admin_name() {
+    # $ADMIN_DN=$1
+    ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}mdb,cn=config
+changetype: modify
+replace: olcRootDN
+olcRootDN: $1
+EOF
 
-#function add_db_root_element() {
-#
-#}
+}
+
+function set_admin_password() {
+    # ADMIN_HASH=$1
+    ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}mdb,cn=config
+changetype: modify
+add: olcRootPW
+olcRootPW: $1
+EOF
+
+}
+
+function add_db_root_element() {
+    # ADMIN_PASSWORD=$1
+    # ADMIN_DN=$2
+    # SUFFIX=$3
+    # ROOT_DC=$4
+    # DESCRIPTION=$4
+    ldapadd -x -w $1 -D $2  -H ldapi:/// <<EOF
+dn: $3
+objectClass: domain
+dc: $4
+description: $5
+EOF
+
+}
 
 
-#/usr/sbin/slapd -F /etc/openldap/slapd.d -u ldap -g ldap -h "ldap:/// ldaps:/// ldapi:///"
-#set_suffix ${SUFFIX}
-#set_admin_name ${ADMIN_USERNAME}
-#set_admin_password ${ADMIN_PASSWORD}
-#add_db_root_element ${ADMIN_DN} ${ADMIN_PASSWORD} ${SUFFIX}
+/usr/sbin/slapd -F /etc/openldap/slapd.d -u ldap -g ldap -h "ldap:/// ldaps:/// ldapi:///"
+set_suffix ${SUFFIX}
+set_admin_name ${ADMIN_USERNAME}
+set_admin_password ${ADMIN_PASSWORD}
+add_db_root_element ${ADMIN_PASSWORD ${ADMIN_DN} ${SUFFIX} ${ROOT_DC} "${DESCRIPTION}"
 
 # stop daemon
 
-# exec daemon
 exit
 
+# exec daemon
+exec /usr/sbin/slapd -F /etc/openldap/slapd.d -u ldap -g ldap -h "ldap:/// ldaps:/// ldapi:///"
+
 """
-sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={2}hdb,cn=config
-changetype: modify
-replace: olcSuffix
-olcSuffix: dc=example,dc=com
 
-sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={2}hdb,cn=config
-changetype: modify
-replace: olcRootDN
-olcRootDN: cn=Manager,dc=example,dc=com
 
-slappasswd
-New password: 
-Re-enter new password: 
-{SSHA}nottherealhashstringthiswontworkuseyourown
 
-sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={2}hdb,cn=config
-changetype: modify
-add: olcRootPW
-olcRootPW: {SSHA}nottherealhashstringthiswontworkuseyourown
 
-ldapadd -x -w secret -D cn=Manager,dc=example,dc=com  -H ldapi:/// <<EOF
-dn: dc=example,dc=com
-objectClass: domain
-dc: example
-description: The Example Company of America
-"""
+
